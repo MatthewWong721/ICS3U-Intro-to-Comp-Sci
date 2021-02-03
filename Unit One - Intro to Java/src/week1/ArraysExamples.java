70785b1848118c7a2138da9b6db6c1826d9fe961
Matthew Wong <mwong1@bayviewglen.ca>
2021-02-03 08:44:13 -0500

    Arrays, Variables and Strings Commit

--- Unit One - Intro to Java/src/week1/ArraysExamples.java
+++ Unit One - Intro to Java/src/week1/ArraysExamples.java
@@ -1,30 +1,37 @@
+  
 package week1;
 
 public class ArraysExamples {
-    public static void main(String[] args){
-        exampleOne();
-        double[] arr = {2.3,1.4,7.6,0.4};
-        exampleTwo(arr);
+  public static void main(String[] args) {
+    exampleOne();
+    double[] arr = {2.3, 1.4, 7.6, 0.4};
+    exampleTwo(arr);
+    System.out.println(arr);
+  }
+
+  private static void exampleTwo(double[] arr1) {
+    double sum = 0;
+
+    for (double x : arr1) {
+      x++;
+      System.out.println(x);
     }
 
-    private static void exampleTwo(double[] arr){
-        double sum=0;
-        for(double x:arr){
-            sum+=x;
-        }
-        System.out.println(sum);
+    System.out.println(sum);
+  }
+
+  private static void exampleOne() {
+    int[] arr = new int[10];
+
+    for (int i=0;i<arr.length; i++){
+      arr[i] = (int)(Math.random()*10 + 1);
+    }
+    int sum = 0;
+
+    for (int x : arr){
+      sum += x;
     }
 
-    private static void exampleOne(){
-        int[] arr = new int[10];
-        for(int i=0;i<arr.length;i++){
-            arr[i]=(int)(Math.random()*10+1);
-        }
-        int sum=0;
-
-        for(int x:arr){
-            sum+=x;
-        }
-        System.out.println(sum);
-    }
+    System.out.println(sum);
+  }
 }
--- Unit One - Intro to Java/src/week1/VariablesAndAssignmentOperator.java
+++ Unit One - Intro to Java/src/week1/VariablesAndAssignmentOperator.java
@@ -1,23 +1,35 @@
 package week1;
 
+/**
+ * Using primitive variables in our programs.
+ * Primitive data only store the data and have no extra functionality.
+ * The primitive data types we will be using this year are:
+ * int: integers
+ * double: decimal numbers
+ * boolean: true and false
+ * char:  characters (they have an int that is associated with each character)
+ */
 public class VariablesAndAssignmentOperator {
-    public static void main(String[] args) {
-        exampleOne();
-        exampleTwo();
-    }
+  public static void main(String[] args) {
+    exampleOne();
+    exampleTwo();
+    
+  }
 
-    private static void exampleTwo(){
-        double x =2.3;
-        double y=3.1;
-        double sum = x+y;
+  private static void exampleTwo() {
+    double x = 2.3;       // declare a variable and assign a value to it in one step.
+    double y = 3.1;
+    double sum = x + y;
 
-        System.out.println(sum);
-    }
+    System.out.println(sum);
+  }
 
-    private static void exampleOne() {
-        int numberOne;
-        numberOne = 10;
-        numberOne++;
-        System.out.println(numberOne);
-    }
+  private static void exampleOne() {
+    // variables are used to store data / information
+    int numberOne;    // declare a variable called numberOne that refers to a primitive int
+    numberOne = 10;   // assignment operator -- reads as numberOne is assigned 10
+
+    numberOne = numberOne + 1;  // numberOne is assigned numberOne + 1
+    System.out.println(numberOne);
+  }
 }
